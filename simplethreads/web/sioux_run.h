d867a0d4ed1f5efe81ba54dd06d05fad8ddd34d7
Joseph Wu <kaysoky88@gmail.com>
2014-02-02 17:21:57 -0800

    Diverging from 451repo by removing all assignment specific code.  This leaves just the kernel code.

--- simplethreads/web/sioux_run.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIOUX_RUN_H
-#define SIOUX_RUN_H 1
-
-void web_runloop(const char *host, int port, const char *docroot);
-
-#endif /* SIOUX_RUN_H */
--- simplethreads/web/web_queue.c
+++
@@ -1,5 +0,0 @@
-/*
- * web_queue.c - An empty file. Use it if you wish, don't if you don't.
- * 
- */
-
